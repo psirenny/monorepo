6d49a85f8460ea78033df2cab45b20883b4b7925
psirenny <djtorres0@gmail.com>
2018-09-06 10:05:58 -0400

    chore: debug circleci

--- packages/tools/src/code/cli.babel.js
+++ packages/tools/src/code/cli.babel.js
@@ -3,6 +3,8 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable import/no-extraneous-dependencies */
 
+console.log(require.resolve('@babel/register'));
+
 require('@babel/register')({
   babelrc: false,
   ignore: [modulePath => !modulePath.includes('monorepo/packages')],
