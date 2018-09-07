83684d39d1b96e58370e3814e16d946e9264a8c1
psirenny <djtorres0@gmail.com>
2018-09-07 05:52:34 -0400

    chore(tools): remove hardcoded path from bootstrap script

--- packages/tools/src/code/cli.babel.js
+++ packages/tools/src/code/cli.babel.js
@@ -3,11 +3,13 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable import/no-extraneous-dependencies */
 
-console.log(require.resolve('@babel/register'));
+const path = require('path');
+
+const pkgsDir = path.join(__dirname, '../../..');
 
 require('@babel/register')({
   babelrc: false,
-  ignore: [modulePath => !modulePath.includes('monorepo/packages')],
+  ignore: [modulePath => !modulePath.includes(pkgsDir)],
 });
 
 require('./cli');
