ea828108fdfb0f4065bb3982b44618ed74cc4195
psirenny <djtorres0@gmail.com>
2020-02-27 14:53:50 -0500

    chore: update dependencies

--- packages/eslint-config-react/src/index.js
+++ packages/eslint-config-react/src/index.js
@@ -3,6 +3,8 @@
 export default {
   extends: 'eslint-config-airbnb',
   rules: {
+    'react/forbid-component-props': [2, { forbid: ['style'] }],
+    'react/forbid-elements': [2, { forbid: ['style'] }],
     'react/jsx-filename-extension': [0],
     'react/jsx-props-no-spreading': [0],
   },
